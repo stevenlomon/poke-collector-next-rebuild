3aa31080a14de7dd89bc2ef090b76b2da6375ef2
StevenLomon <steven.cesario@proton.me>
2026-06-30 04:36:10 +0200

    types.ts with interfaces

---
+++ app/lib/types.ts
@@ -0,0 +1,15 @@
+// The shape of the data we use coming directly from the PokeWallet API. Can easily be expanded
+export interface PokemonCard {
+  id: string;
+  // We mirror the nested structure of the API response
+  card_info: {
+    id: string;
+    set_name?: string;
+  };
+}
+
+// The shape of the data strictly inside our Context state
+export interface CollectionItem extends PokemonCard {
+  amount: number;
+  isNew: boolean;
+}
