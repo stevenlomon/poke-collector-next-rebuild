5ae2a6c3fd1b91e180540080a2a2ac671cf4f22b
StevenLomon <steven.cesario@proton.me>
2026-06-30 14:21:13 +0200

    CardImage integrated into CollectionItem

--- src/components/CollectionItem.tsx
+++ src/components/CollectionItem.tsx
@@ -2,8 +2,9 @@
 
 import { type CollectionItem as CardDataType } from "@/lib/types"
 import Link from "next/link"
+import CardImage from "./CardImage"
 
-// Define the props the component expects, reusing our context types
+// Define the props the component expects, reusing our context type
 interface CollectionItemProps {
   card: CardDataType;
   onIncrease: (id: string) => void;
@@ -13,9 +14,12 @@ interface CollectionItemProps {
 
 const CollectionItem = ({ card, onIncrease, onDecrease, onDelete }: CollectionItemProps) => {
   return (
-    // Re-used from the Vite React SPA. The Link will need to be updated
+    // Re-used from the Vite React SPA. The Link is the only thing updated and is now Next native
     <div className={`collection-item ${card.isNew ? 'animate-in' : ''}`}>
-      {/* <Link href={`/card/${card.id}`} state={{ cardData: card }} ><CardImage cardId={card.id} cardName={card.card_info.name} /></Link> */}
+      {/* This.. */}
+      {/* <Link to={`/card/${card.id}`} state={{ cardData: card }} ><CardImage cardId={card.id} cardName={card.card_info.name} /></Link> */}
+      {/* ..becomes this! With a new fallback on the card name. `state` is not a property for Next.js Link and the "Backpack strat" from the original version is taken care of entirely by Next; Next.js *is* the backpack now haha! */}
+      <Link href={`/card/${card.id}`} ><CardImage cardId={card.id} cardName={card.card_info.name || "Pokemon Card"} /></Link>
 
       <div className="inventory-controls">
         {/* The onClicks need to be arrow functions since we're passing arguments to keep them as function references! 🚀 */}
