6305b827cb94fdea32d4b24f852f05cfa625435a
StevenLomon <steven.cesario@proton.me>
2026-07-01 13:19:30 +0200

    search page fetch + search page error component

---
+++ src/app/search/error.tsx
@@ -0,0 +1,27 @@
+'use client'
+// Client component that is purely responsible for catching errors and displaying them!
+// Under the hood it uses React Error Boundaries which is advanced React I've never had a reason to touch haha
+
+import { useEffect } from "react";
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }, reset: () => void }) { // "Expect an object that has everything a standard Error has, AND also allow it to have a digest property."
+  useEffect(() => {
+    // Here we can log the error to the console or send it to services like Sentry or Datadog
+    console.error("Caught by Next.js Error Boundary:", error)
+  }, [error]);
+
+  return (
+    <div className="flex flex-col items-center justify-center p-10 text-center">
+      <h2>Oh no! A wild error appeared!</h2>
+      <p className="text-gray-500 my-4">{error.message || "Failed to fetch Pokémon data."}</p>
+      
+      {/* The reset function tells Next.js to re-run the server component and try again! */}
+      <button
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={() => reset()}
+      >
+        Try Again
+      </button>
+    </div>
+  )
+} 
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -8,6 +8,27 @@ import { searchCards } from "@/lib/api";
 import SearchResultItem from "@/components/SearchResultItem";
 
 export default async function SearchPage({ searchParams }: { searchParams: Promise<{ q?: string, page?: string, limit?: string }>;}) {
+  const params = await searchParams;
+  const query = params.q;
+  const page = parseInt(params.page || "1");
+  const limit = parseInt(params.limit || "20");
+
+  // Our empty state; returned instanty, no fetching needed
+  if (!query) {
+    return (
+      <div>
+        <h2>Looking for Pokémon?</h2>
+        <p>Use the search bar to find cards to add to your collection!</p>
+      </div>
+    )
+  }
+
+  // The actual fetch now is just pure server-side awaiting! No useEffect and no loading states related to the data fetching!!
+  // If it fails, Next.js automatically catches it for us in errors.tsx!
+  const data = await searchCards(query, page, limit);
+  const searchResults = data.results;
+  const pagination = data.pagination;
+
   return (
     <div>SearchPage</div>
   )
